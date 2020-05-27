7768dfa1d37ed40a093f631adc1a7689f459c98e
stephen.rodrigues@bnedloudcloud.com <stephen.rodrigues@bnedloudcloud.com>
2020-05-27 20:28:29 +0530

    First awsnodejs commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Hello , Welcome to AWSNODEJS")
+    console.log("Hello World")
+})
+
+const port = process.env.port || 3000
+
+app.listen(port ,()=>{
+    console.log("Server started on ", port)
+})
