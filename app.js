b66e6cca520efd86e3ee1e61e692171e7c32d49e
stephen.rodrigues@bnedloudcloud.com <stephen.rodrigues@bnedloudcloud.com>
2020-08-13 15:45:52 +0530

    s

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/',(req,res)=>{
     console.log("Hello World")
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.listen(port ,()=>{
     console.log("Server started on ", port)
